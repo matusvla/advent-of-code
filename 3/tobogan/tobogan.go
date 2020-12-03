0e9fb115ad999e1dbb6fea990385dc585613e09e
Vladislav Matus <vladislav.matus@deutsche-boerse.com>
2020-12-03 09:03:59 +0100

    Exercise 3 shortened

--- 3/tobogan/tobogan.go
+++ 3/tobogan/tobogan.go
@@ -6,8 +6,5 @@ const (
 
 func CheckTree(line string, lineNo int, step float32) bool {
 	gotoPlace := step * float32(lineNo)
-	if gotoPlace != float32(int(gotoPlace)) {
-		return false
-	}
-	return line[int(gotoPlace)%len(line)] == tree
+	return gotoPlace == float32(int(gotoPlace)) && line[int(gotoPlace)%len(line)] == tree
 }
